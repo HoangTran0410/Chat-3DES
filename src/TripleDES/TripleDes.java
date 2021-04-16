9008f67d042dc281c1e4d6afa02b505505afb0a5
Hoàng Lưu <hoang@Hoangs-MacBook-Pro.local>
2021-04-16 16:56:02 +0700

    [Feature] TRIPLE DES COMPLETED. easy game 8-)

--- src/TripleDES/TripleDes.java
+++ src/TripleDES/TripleDes.java
@@ -3,21 +3,38 @@ package TripleDES;
 public class TripleDes {
     Des[] _desInstances = new Des[]{new Des(), new Des(), new Des()};
     byte[][] _desKeys = new byte[3][56];
-    byte[][] _crypts = new byte[3][64];
 
-    public void Encrypt(byte[] plain, byte[][] inputKeys) {
+    public byte[] Encrypt(byte[] plain, byte[][] inputKeys) {
         for (int i = 0; i < 3; i++) {
             _desKeys[i] = Des.CreateSingleKey(inputKeys[i]);
         }
 
-        _crypts[0] = _desInstances[0].Encrypt(plain, _desKeys[0]);
-        _crypts[1] = _desInstances[1].Decrypt(_crypts[0], _desKeys[1]);
+        byte[] crypt;
+        crypt = _desInstances[0].Encrypt(plain, _desKeys[0]);
+        crypt = _desInstances[1].Decrypt(crypt, _desKeys[1]);
+        crypt = _desInstances[2].Encrypt(crypt, _desKeys[2]);
+
+        return crypt;
+    }
+
+    public byte[] Decrypt(byte[] crypt, byte[][] inputKeys) {
+        for (int i = 0; i < 3; i++) {
+            _desKeys[i] = Des.CreateSingleKey(inputKeys[i]);
+        }
+
+        byte[] plain;
+        plain = _desInstances[0].Decrypt(crypt, _desKeys[2]);
+        plain = _desInstances[1].Encrypt(plain, _desKeys[1]);
+        plain = _desInstances[2].Decrypt(plain, _desKeys[0]);
+
+        return plain;
     }
 
     public static void main(String[] args) {
         TripleDes tripleDes = new TripleDes();
         byte[][] inputKeys = new byte[][] {"m8oemrkt".getBytes(), "!ms_leot".getBytes(), "2mgl@ao!".getBytes()};
         String plain = "Let's go to the beach";
-        tripleDes.Encrypt(plain.getBytes(), inputKeys);
+        byte[] encrypted = tripleDes.Encrypt(plain.getBytes(), inputKeys);
+        byte[] decrypted = tripleDes.Decrypt(encrypted, inputKeys);
     }
 }
